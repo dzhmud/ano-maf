4fbe8aa2a63be657eba9ec4abacfef6215db23e4
Alexander Osadchy <alexandr.osadchy@gmail.com>
2009-11-24 13:58:24 +0000

    Added serialVersionUID

--- java/net/anotheria/maf/ActionFactoryException.java
+++ java/net/anotheria/maf/ActionFactoryException.java
@@ -1,7 +1,13 @@
 package net.anotheria.maf;
 
-public class ActionFactoryException extends Exception{
-	public ActionFactoryException(Exception source){
-		super("Action instantiation failed because: "+source.getMessage(), source);
+public class ActionFactoryException extends Exception {
+
+	/**
+	 * Basic serialVersionUID variable.
+	 */
+	private static final long serialVersionUID = 738036982942005924L;
+
+	public ActionFactoryException(Exception source) {
+		super("Action instantiation failed because: " + source.getMessage(), source);
 	}
 }
