04d221a8f753778e0af161313eedc9e76981fdc3
Vitaliy Zhovtiuk <vzhovtiuk@anotheria.net>
2010-01-13 20:12:27 +0000

    Model Bean mapper

--- java/net/anotheria/maf/util/ModelObjectMapper.java
+++ java/net/anotheria/maf/util/ModelObjectMapper.java
@@ -20,6 +20,12 @@ import java.util.Map;
 public final class ModelObjectMapper {
 
 	/**
+	 * Default hidden constructor
+	 */
+	private ModelObjectMapper() {
+	}
+
+	/**
 	 * Map request parameters to model bean.
 	 * </p>
 	 * <p>
@@ -30,7 +36,7 @@ public final class ModelObjectMapper {
 	 * @param req http request object
 	 * @param destination given model bean
 	 */
-	public static void map(HttpServletRequest req, Object destination) {
+	public static void map(final HttpServletRequest req, final Object destination) {
 		final Map<String, Object> parameterMap = new HashMap<String, Object>(req.getParameterMap());
 		ValueObjectMapperUtil.map(parameterMap, destination);
 	}
