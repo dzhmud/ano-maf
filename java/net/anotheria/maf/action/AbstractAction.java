71c90f2584bfb01f174604363474eb4d1fd0d808
Leon Rosenberg <leon.rosenberg@anotheria.net>
2010-10-04 22:19:43 +0000

    CS

--- java/net/anotheria/maf/action/AbstractAction.java
+++ java/net/anotheria/maf/action/AbstractAction.java
@@ -3,17 +3,22 @@ package net.anotheria.maf.action;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Abstract action class which can be used as parent of all action classes.
+ * @author another
+ *
+ */
 public abstract class AbstractAction implements Action{
 
 	@Override
 	public void preProcess(ActionMapping mapping, HttpServletRequest req,
-			HttpServletResponse res) throws Exception {
+			HttpServletResponse res) {
 		
 	}
 
 	@Override
 	public void postProcess(ActionMapping mapping, HttpServletRequest req,
-			HttpServletResponse res) throws Exception {
+			HttpServletResponse res) {
 		
 	}
 
